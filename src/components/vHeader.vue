03c9c43f4f604669f2a1e104c87269a49097dc9c
zzq <Keen_Team@163.com>
2021-05-20 15:20:41 +0800

    Vue2.0

---
+++ src/components/vHeader.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <div class="header">
+    <img src="../assets/identify.png" @click="clickIcon">
+    <span>爱新闻</span>
+    <img src="../assets/search.png" @click="toSearch">
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clickIcon() {
+      this.$store.state.showMy = true
+      this.$store.state.showLogin = true
+    },
+
+    toSearch() {
+      this.$router.push('/search')
+    },
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.header {
+  width: 100%;
+  height: 1.2rem;
+  background-color: #888888;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 2px;
+  z-index: 99;
+
+}
+  img {
+    width: .67rem;
+    height: .67rem;
+    padding: .4rem;
+  }
+
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <my v-show="showMy"></my>
+    <v-header class="pos-header"></v-header>
+    <v-nav v-bind:msg="msg"></v-nav>
+  </div>
+</template>
+
+<script>
+import my from '@/components/my'
+import vHeader from '@/components/vHeader'
+import vNav from '@/components/vNav'
+
+export default {
+
+ data() {
+    return{
+      msg: [
+         {src: require('../assets/qinliugan.jpg')},
+         {src: require('../assets/huanqiu.jpg')},
+         {src: require('../assets/aierlan.jpg')},
+      ]
+    }
+  },
+
+  created() {
+    this.$store.commit('muShowMy', false)
+  },
+  components: {
+    vHeader,
+    vNav,
+    my
+  },
+  computed: {
+    showMy() {
+      return this.$store.state.showMy
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.pos-header {
+  position: fixed;
+  top: 0;
+}
+</style>
